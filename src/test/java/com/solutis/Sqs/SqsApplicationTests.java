fc44cd086aaa2e9bf354fc5f303fbdc3f444fc15
Maria Eduarda <mariaeduardasoter@gmail.com>
2024-09-30 05:39:27 -0300

    atividade

---
+++ src/test/java/com/solutis/Sqs/SqsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solutis.Sqs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
